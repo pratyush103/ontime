5abf1bcd89039f78c1f68c270d58199015f57025
Pratyush <pratyushlandekar103@gmail.com>
2025-01-17 20:00:53 +0530

    deploy: deploy config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = (phase: string): NextConfig => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+  return {
+    env: {
+      DATABASE_URL: isDev
+        ? process.env.DATABASE_URL
+        : process.env.POSTGRES_PRISMA_URL,
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    },
+  };
 };
 
 export default nextConfig;
